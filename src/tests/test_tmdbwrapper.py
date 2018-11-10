6bee4d1a5325a35cfcfd5d8e4600e2a915153cfa
chris <chrispyprogrammer@gmail.com>
2018-11-10 19:18:50 +0200

    added fixture in test_tmdbwrapper / line 37 assertion error need to be fixed

--- src/tests/test_tmdbwrapper.py
+++ src/tests/test_tmdbwrapper.py
@@ -1,12 +1,37 @@
+
+from pytest import fixture
 from tmdbwrapper import TV
 
 
 
+
+@fixture
+# to return test data / see line 34
+def tv_keys():
+
+    return [
+
+    'id',
+    'origin_country',
+    'poster_path',
+    'name',
+    'overview',
+    'popularity',
+    'backdrop_path',
+    'first_air_date',
+    'vote_count',
+    'vote_average'
+
+    ]
+
+
+
 # testing the API call
-def test_tv_info():
+def test_tv_info(tv_keys):
 
     tv_instance = TV(1396)
     response = tv_instance.info()
 
     assert isinstance(response, dict)
     assert response['id'] == 1396, "The ID is not inside the response"
+    assert set(tv_keys).issubset(response.keys()), "some keys are missing in this response"
