dd7682dffb31328ed0a6ea18880b748cb12ceeab
chris <chrispyprogrammer@gmail.com>
2018-11-10 23:33:51 +0200

    work in progress

--- src/wrapper_testrun.py
+++ src/wrapper_testrun.py
@@ -11,10 +11,9 @@ popular = TV.popular()
 
 for number, show in enumerate(popular['results'], start=1):
 
-        print(
+    print("{num}. {name} - {pop}".format(num  = number,
+                                         name = show['name'],
+                                         pop  = show['popularity']))
 
-            f"{num   = number}. "\
-            f"{name  = show['name']} - "\
-            f"{pop   = show['popularity']}"
 
-            )
+
